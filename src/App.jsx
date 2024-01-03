dea45437c9ce82b96cd9d0ef33a699d279c0652e
Hans Christian Herrera <hanschristian.herrera@wvsu.edu.ph>
2024-01-03 17:06:03 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import '@ionic/react/css/core.css';
+import { setupIonicReact } from '@ionic/react';
+import { IonButton, IonDatetime } from '@ionic/react';
+
+// import React from 'react';
+// import { IonButton, IonDatetime } from '@ionic/react';
+
+setupIonicReact();
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+
+
+      <IonDatetime></IonDatetime>
+      <IonButton fill="clear">Start</IonButton>
+    </>
+  )
+}
+
+export default App
